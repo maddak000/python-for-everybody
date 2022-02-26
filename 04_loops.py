b34cf75b201166b908c51df857b0657652e726dd
maddak000 <marcin.dworak@gmail.com>
2022-02-26 10:28:23 +0100

    more loops

--- 04_loops.py
+++ 04_loops.py
@@ -8,3 +8,13 @@ while n < 5:
 print("for loop")
 for i in [2, 1, 5]:
     print(i)
+
+smallest = None
+li = [9, 4, 15, 234, 5, 1]
+print(li)
+for i in li:
+    if smallest is None:
+        smallest = i
+    elif i < smallest:
+        smallest = i
+print("smallest: ", smallest)
