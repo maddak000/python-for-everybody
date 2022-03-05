0cff4abc64183656af219a9708c4b8e8629d993a
maddak000 <marcin.dworak@gmail.com>
2022-03-05 20:57:30 +0100

    web services xml

--- 15_web_services.py
+++ 15_web_services.py
@@ -1,1 +1,15 @@
 import xml.etree.ElementTree as ET
+
+data = """
+<person>
+    <name>Check</name>
+    <phone type="intl">
+        +1 734 303 45456
+    </phone>
+    <email hide="yes"/>
+</person>
+"""
+
+tree = ET.fromstring(data)
+print("Name:", tree.find("name").text)
+print("Attr:", tree.find("email").get("hide"))
