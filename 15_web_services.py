ba825271e8e92584c95a737b6cf058c62bdf04eb
maddak000 <marcin.dworak@gmail.com>
2022-03-06 10:28:31 +0100

    web services extra example

--- 15_web_services.py
+++ 15_web_services.py
@@ -31,3 +31,18 @@ info = json.loads(data)
 print("Name:", info["name"])
 print("Hide:", info["email"]["hide"])
 
+data = '''
+  [
+    { "id" : "001",
+      "x" : "2",
+     "name" : "Quincy"
+    } ,
+    { "id" : "009",
+      "x" : "7",
+      "name" : "Mrugesh"
+    }
+  ]
+'''
+info = json.loads(data)
+print(info[1]['name'])
+
