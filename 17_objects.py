d4e7d62c9d97bf5f8b265fc46b1c0a7b945c7743
maddak000 <marcin.dworak@gmail.com>
2022-03-13 16:09:01 +0100

    adjusted objects example

--- 17_objects.py
+++ 17_objects.py
@@ -17,3 +17,19 @@ m = PartyAnimal('Miya')
 q.party()
 m.party()
 q.party()
+
+
+class FootballFan(PartyAnimal):
+    points = 0
+
+    def touchdown(self):
+        self.points += 7
+        self.party()
+        print(self.name, "points", self.points)
+
+
+s = PartyAnimal("Sally")
+s.party()
+j = FootballFan("Jim")
+j.party()
+j.touchdown()
